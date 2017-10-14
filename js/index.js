c959721962162ada153f73ff2088a22e4262488b
karanagarwal17 <karanagarwal1702@gmail.com>
2017-10-15 00:10:19 +0530

    implemented callbacks

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
+var logo = document.getElementById("logo");
+var text = document.getElementById("text");
+var footer = document.getElementById("footer");
+var wrapper = document.getElementById("wrapper");
+
 function show() {
   var el = document.getElementById("wrapper");
   el.style.opacity = 1;
@@ -8,8 +13,7 @@ function hide() {
   el.style.opacity = 0;
 }
 
-function fadeIn(el) {
-
+function fadeIn(el, callback) {
   var tick = function() {
     el.style.opacity = +el.style.opacity + 0.01;
 
@@ -19,14 +23,20 @@ function fadeIn(el) {
   };
 
   tick();
+  if(callback) {
+    callback();
+  }
 }
 
-function move(elem) {
+function move(elem, callback) {
   var pos = 140;
   var id = setInterval(frame, 5);
   function frame() {
     if (pos == 10) {
       clearInterval(id);
+      if(callback){
+        callback();
+      }
     } else {
       pos--;
       elem.style.marginTop = pos + 'px';
@@ -35,17 +45,16 @@ function move(elem) {
 }
 
 function todo() {
-  var logo = document.getElementById("logo");
   logo.style.opacity = 0;
-  var text = document.getElementById("text");
   text.style.opacity = 0;
-  var footer = document.getElementById("footer");
   footer.style.opacity = 0;
-  var wrapper = document.getElementById("wrapper");
   wrapper.style.opacity = 1;
 
-  fadeIn(logo);
-  move(logo);
-  fadeIn(text);
-  fadeIn(footer);
+  fadeIn(logo, function(){
+    move(logo, function(){
+      fadeIn(text, function() {
+        fadeIn(footer);
+      });
+    });
+  });
 }
