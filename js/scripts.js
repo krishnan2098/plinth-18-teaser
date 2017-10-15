927fa57cacd38e4adbdbc1b399da4c67da628ebb
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2017-10-16 02:55:35 +0530

    tried 30fps

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ window.onload = function() {
 
   window.requestAnimFrame = (function() {
     return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function(callback) {
-      window.setTimeout(callback, 1000 / 60);
+      window.setTimeout(callback, 1000 / 30);
     };
   })();
 
